5231acca1da940ccc427ee9c653c67b2d878e491
Andre Fernando Quaresma da Silva <andre.quaresma@AKAER.com.br>
2024-05-29 12:42:23 -0300

    refactor: use of generics views

--- curso/urls.py
+++ curso/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CursoAPIView, AvaliacaoAPIView
+from .views import CursosAPIViews, CursoAPIViews, AvaliacoesAPIViews, AvaliacaoAPIViews
 
 
 urlpatterns = [
-    path('cursos/', CursoAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+    path('cursos/', CursosAPIViews.as_view(), name='cursos'),
+    path('cursos/<int:pk>', CursoAPIViews.as_view(), name='cursos'),
+    path('avaliacoes/', AvaliacoesAPIViews.as_view(), name='avaliacoes'),
+    path('avaliacoes/<int:pk>', AvaliacaoAPIViews.as_view(), name='avaliacoes'),
 ]
