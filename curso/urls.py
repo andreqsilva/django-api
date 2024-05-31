9609b1782697aa6125dbf07e5cca98039198fd21
andreqsilva <andrefernandoqsilva31@gmail.com>
2024-05-31 19:22:40 -0300

    feat: add get_queryset and get_objecto to generic views

--- curso/urls.py
+++ curso/urls.py
@@ -4,7 +4,9 @@ from .views import CursosAPIViews, CursoAPIViews, AvaliacoesAPIViews, AvaliacaoA
 
 urlpatterns = [
     path('cursos/', CursosAPIViews.as_view(), name='cursos'),
-    path('cursos/<int:pk>', CursoAPIViews.as_view(), name='cursos'),
+    path('cursos/<int:curso_pk>', CursoAPIViews.as_view(), name='cursos'),
+    path('cursos/<int:curso_pk>/avaliacoes', AvaliacoesAPIViews.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>', AvaliacaoAPIViews.as_view(), name='curso_avaliacao'),
+    path('avaliacoes/<int:avaliacao_pk>', AvaliacaoAPIViews.as_view(), name='avaliacoes'),
     path('avaliacoes/', AvaliacoesAPIViews.as_view(), name='avaliacoes'),
-    path('avaliacoes/<int:pk>', AvaliacaoAPIViews.as_view(), name='avaliacoes'),
 ]
--- curso/views.py
+++ curso/views.py
@@ -2,6 +2,10 @@ from rest_framework import generics
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
+from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class CursosAPIViews(generics.ListCreateAPIView):
@@ -16,6 +20,18 @@ class AvaliacoesAPIViews(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    # get_queryset pega varioes elementos
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'): # pega o elemento 'curso_pk' do endpoint
+            return self.queryset.filter(curso_id = self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+
 class AvaliacaoAPIViews(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    # get_object pega um elemento específico
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'), pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
